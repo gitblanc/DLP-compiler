8d23bb09742344aa1f193374db49333c9b37f77f
gitblanc <UO285176@uniovi.es>
2023-03-27 10:05:21 +0200

    attribute grammar

--- inicio.vsc/src/ast/AbstractExpresion.java
+++ inicio.vsc/src/ast/AbstractExpresion.java
@@ -5,5 +5,22 @@
 package ast;
 
 public abstract class AbstractExpresion extends AbstractAST implements Expresion {
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
 
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+	public void setModificable(boolean modificable) {
+		this.modificable = modificable;
+	}
+
+	public boolean isModificable() {
+		return modificable;
+	}
+
+	private Tipo tipo;
+	private boolean modificable;
 }
--- inicio.vsc/src/ast/Expresion.java
+++ inicio.vsc/src/ast/Expresion.java
@@ -5,5 +5,11 @@
 package ast;
 
 public interface Expresion extends AST {
+	public void setTipo(Tipo tipo);
 
+    public Tipo getTipo();
+
+    public void setModificable(boolean modificable);
+
+    public boolean isModificable();
 }
